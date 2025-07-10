904ef9cc3fe0551c527d47f0028900af3cab5692
Arthur Amaral <arthuramaral6@gmail.com>
2025-07-10 07:12:50 -0300

    config: adding pylint to project

---
+++ ex_pylint.py
@@ -0,0 +1,11 @@
+'''
+O pylint é uma biblioteca para ajudar escrever um código melhor (ver documentação).
+Comentario de documentacao, sem ele o pylint acusa "erro"
+podemos ver e editar as regras que o pylint adota no seu arquivo de configuracao.
+para acessar esse arquivo, no terminal damos o comando pylint --generate-rcfile > .pylintrc
+'''
+
+print('Ola mundo')
+
+def teste_nome():
+  pass
